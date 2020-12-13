188e1203b25eec7484a04ea8862cdff4571f4fb2
taniafilipa33 <com.tani33@gmail.com>
2020-12-13 23:47:58 +0000

    update

--- projectsPOO_1920/tarefa1Extra.sh
+++ projectsPOO_1920/tarefa1Extra.sh
@@ -1,13 +1,22 @@
-find -name *.java -not -path "./*\_\_MACOSX/*"| sed  's/src.*/src/' | sed 's/\/\w*.java$//'| sed 's/\.\///' | sort -u > output.txt
+find -name *.java -not -path "./*\_\_MACOSX/*"| uniq --check-chars 4 |sed  's/src.*/src/' | sed 's/\/\w*.java$//'| sed 's/\.\///' > output.txt
 while read -r line
 do
 	PP=$(echo "$line" | cut -c -2)
 	cd $PP
 	mkdir -p src/main/java
-	CAM=$(find -name *.java -not -path "./*\_\_MACOSX/*"| sed  's/src.*/src/' | sed 's/\/\w*.java$//'| sed 's/\.\///' | sort -u | xargs -0) 
- 	cp -a "$CAM"/. src/main/java
+	find -name *.java -not -path "./*\_\_MACOSX/*"| sed  's/src.*/src/' | sed 's/\/\w*.java$//'| sed 's/\.\///' | sort -u > ot.txt  
+ 	while read -r line
+ 	do
+ 		cp -a "$line"/. src/main/java
+
+	done < ot.txt
+	find src/main/java/ -type f ! -name '*.java' -delete 
+	cd src/main/java/
+	rm -rf `ls -d */`
+	cd ..
+	cd ..
+	cd ..
 	PAS=$(basename "$PWD")
-	find src/main/java/ -type f ! -name '*.java' -delete
 	echo '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
 	<modelVersion>4.0.0</modelVersion>
 	<groupId>groupId</groupId>
